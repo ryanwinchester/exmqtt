515ca044444d42fb376bcab699c46df51301cab4
Riccardo Degan <riccardo.degan@inkwelldata.com>
2023-09-20 14:24:50 +0200

    Set version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExMQTT.MixProject do
   def project do
     [
       app: :exmqtt,
-      version: "1.0.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       description: "An Elixir wrapper for erlang emqtt lib",
       package: package(),
